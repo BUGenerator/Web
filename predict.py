1d262f550aaa3b18e2baf1116d2605c6526aa7c8
Phy <git@phy25.com>
2018-11-12 15:40:13 -0500

    Try to fix untrained module
    No training configuration found in save file: the model was *not* compiled. Compile it manually.

--- predict.py
+++ predict.py
@@ -4,6 +4,16 @@ from keras import models
 from skimage.morphology import binary_opening, disk
 
 fullres_model = models.load_model("model_fullres_keras.h5")
+if os.environ.get('SHIPDETECTION_BROKEN_MODEL'):
+    from keras.optimizers import Adam
+    import keras.backend as K
+    def IoU(y_true, y_pred, eps=1e-6):
+        if np.max(y_true) == 0.0:
+            return IoU(1-y_true, 1-y_pred) ## empty image; calc IoU of zeros
+        intersection = K.sum(y_true * y_pred, axis=[1,2,3])
+        union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3]) - intersection
+        return -K.mean( (intersection + eps) / (union + eps), axis=0)
+    fullres_model.compile(optimizer=Adam(1e-3, decay=1e-6), loss=IoU, metrics=['binary_accuracy'])
 
 def _raw_prediction(img):
     img = np.expand_dims(img, 0)/255.0
