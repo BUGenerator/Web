6b74a48da782f673407d559d0e8c78118abde58a
feiyue33 <xli33@bu.edu>
2018-11-26 22:40:35 -0500

    fix uppercase extension bug

--- upload.py
+++ upload.py
@@ -16,7 +16,7 @@ ALLOWED_EXTENSIONS = set(['jpg', 'png', 'jpeg'])
 
 # allowed file type (
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
